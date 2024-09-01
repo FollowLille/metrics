882c622e3599db407438982368b34842ae19abb9
Maksim Nikolay <mvnikolay@gmail.com>
2024-09-01 18:23:24 +0500

    test for linter

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -93,17 +93,17 @@ type MemStorage struct {
 	counters map[string]int64
 }
 
-func NewMemStorage() *MemStorage {
-	return &MemStorage{
-		gauges:   make(map[string]float64),
-		counters: make(map[string]int64),
-	}
-}
+//func NewMemStorage() *MemStorage {
+//	return &MemStorage{
+//		gauges:   make(map[string]float64),
+//		counters: make(map[string]int64),
+//	}
+//}
 
-func (s *MemStorage) UpdateGauge(name string, value float64) {
-	s.gauges[name] = value
-}
-
-func (s *MemStorage) UpdateCounter(name string, value int64) {
-	s.counters[name] += value
-}
+//func (s *MemStorage) UpdateGauge(name string, value float64) {
+//	s.gauges[name] = value
+//}
+//
+//func (s *MemStorage) UpdateCounter(name string, value int64) {
+//	s.counters[name] += value
+//}
